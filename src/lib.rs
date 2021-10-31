6fa5d97080345a461e660b09e03c67b2412494fd
Thrigger <21341498+Thrigger@users.noreply.github.com>
2021-10-31 15:07:12 +0100

    updated Index_of trait to be applicable to slices also

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+mod index_of;
+
 /// Finds a pattern between the two specified strings
 ///
 /// Returns the first occurens of a string between the two specified patterns
@@ -85,49 +87,9 @@ pub fn find_max(input: &Vec<i64>) -> i64 {
     largest
 }
 
-trait IndexOf<T> {
-    fn index_of(&self, element: T) -> Option<usize>;
-    fn index_of_reversed(&self, element: T) -> Option<usize>;
-}
-
-impl<T: std::cmp::PartialEq> IndexOf<T> for Vec<T> {
-    fn index_of(&self, element: T) -> Option<usize> {
-        for (i, each) in self.iter().enumerate() {
-            if *each == element {
-                return Some(i);
-            }
-        }
-        None
-    }
-    fn index_of_reversed(&self, element: T) -> Option<usize> {
-        for (i, each) in self.iter().rev().enumerate() {
-            if *each == element {
-                return Some(self.len() - i - 1);
-            }
-        }
-        None
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_index_of() {
-        let a = vec![1, 2, 3, 4, 5, 5, 4, 3, 2];
-        let b = vec!["a", "abc", "ABC", "A"];
-        let c = vec!['a', 'b', 'c', 'a'];
-        assert_eq!(a.index_of(3), Some(2));
-        assert_eq!(a.index_of(6), None);
-        assert_eq!(a.index_of_reversed(3), Some(7));
-        assert_eq!(b.index_of("A"), Some(3));
-        assert_eq!(b.index_of("Abc"), None);
-        assert_eq!(b.index_of_reversed("a"), Some(0));
-        assert_eq!(c.index_of('a'), Some(0));
-        assert_eq!(c.index_of('1'), None);
-        assert_eq!(c.index_of_reversed('a'), Some(3));
-    }
 
     #[test]
     fn test_find_between() {
