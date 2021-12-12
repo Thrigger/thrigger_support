709c9fd24bbaa225fd77f6b7d50ee6d65fc2cd11
Thrigger <21341498+Thrigger@users.noreply.github.com>
2021-12-12 15:39:14 +0100

    Added binary to u32 function
    - Takes a chars vector and returns the value as a u32
    - Added simple unittest
    - Didnt do any error handeling

--- src/data_conversions.rs
+++ src/data_conversions.rs
@@ -25,4 +25,25 @@ fn string_to_str_vec(input: &String) -> Vec<&str> {
         _=>Some(s),}).collect()
 }
 
+fn binary_to_u32(input: &Vec<char>) -> u32 {
+    let mut out = 0:
+    let mut multiplier = 1;
+    for each in reversed(input) {
+        out += multiplier * each.to_digit().unwrap();
+        multiplier *= 2
+    }
+    out
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_binary_to_u32() {
+        assert_eq!(binary_to_u32(vec!['1','1','1','1']),15);
+        assert_eq!(binary_to_u32(vec!['1','0','0','1']),9);
+    }
+}
 
